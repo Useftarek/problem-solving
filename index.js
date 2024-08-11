096c6ac2848b76e0b2674e3a96c4e9a318da9dc4
Useftarek <useftarek2@gmail.com>
2024-08-11 19:16:42 +0200

    done

---
+++ index.js
@@ -0,0 +1,108 @@
+// 1-
+// let num = +prompt("Check Number");
+
+// if (num<=100) {
+//     console.log("number is less than or equal to 100")
+// }
+
+// else {
+//     console.log("number is greater than 100")
+// }
+
+// -2
+
+
+
+
+// -3
+
+// let passwordnum = +prompt("Enter Password")
+
+
+//  if (passwordnum == 123456) {
+//     alert(('confirm password'));
+//  }
+
+//  else {
+//     alert(('vaild password'));
+//  }
+
+
+// -4
+
+
+
+// -5
+
+// let num = +prompt('check number')
+
+// if (num > 0) {
+//     alert('number is postive')
+// } else if (num < 0)  {
+//     alert('number is negative')
+// } else {
+//     alert ('number is zero')
+// }
+
+// -6
+
+// let verifying = +prompt('Verfing Academic')
+
+// if (verifying > 80) {
+//     alert("A")
+// } else if (verifying > 70) {
+//     alert('C')
+// } 
+// else if (verifying > 60) {
+//     alert('D')
+// } 
+// else {
+//     alert ('F')
+// }
+
+
+// 7-
+
+// let temperature =+prompt('Check Temperature temp')
+
+// if (temperature > 30) {
+//     alert("it's hot")
+// } else {
+//     alert("it's cold");
+// }
+
+// 8-
+
+// let num =+prompt('Check Num odd or even')
+
+// if (num % 2 == 0) {
+//     alert('num even')
+// }
+
+// else {
+//     alert('num odd')
+// }
+
+
+// 9
+
+// let num =+prompt('Check Num odd or even');
+// let betterthan =prompt('check age');
+
+// if (num % 2 == 0) {
+//     alert('num even')
+// } else if (num % 2 ) {
+//     alert('num odd')
+// } 
+
+// if (betterthan > 20) {
+//     alert("You can enter the university")
+// } else {
+//     alert ("You can't enter the university")
+// } 
+
+
+
+
+
+
